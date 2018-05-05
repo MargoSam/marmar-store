c4cdee2d0db77435bbbd6a5f5118a57a69872c62
MarMar <samokhina.margo@gmail.com>
2018-05-05 17:48:50 +0300

    Part 2

---
+++ src/com/marmar/coursework/product/ProductNameComparator.java
@@ -0,0 +1,10 @@
+package com.marmar.coursework.product;
+
+import java.util.Comparator;
+
+public class ProductNameComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product product1, Product product2) {
+        return product1.getName().compareTo(product2.getName());
+    }
+}
